c6dc45af5da14a69f54d289548cc9ed3e5feefd3
mcatoeff@gmail.com <mcatoeff@gmail.com>
2019-05-01 22:05:23 -0700

    update 004

--- server.js
+++ server.js
@@ -3,15 +3,43 @@
 
 require('dotenv').config({silent: true});
 
-var server = require('./app');
+
+
+
 var port = process.env.PORT || 3000;
 
-var startServer = server.listen(port, function() {
-  console.log('Server running on port: %d', port);
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/post-api-routes.js")(app);
+require("./routes/author-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
 
+
+
+
+
+
+
+
 function close() {
-  startServer.close();
+  app.close();
 }
 
 module.exports = {
